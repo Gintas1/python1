6c4cef6ecb50701dc7072249eab45d76a4ab8e60
Gintas1 <gintautas.kras@gmail.com>
2014-04-06 16:36:10 +0300

    buvo keičiamas kodas, kad atitiktų standartus

--- pirma.py
+++ pirma.py
@@ -2,7 +2,9 @@ import glob
 import sys
 import pygame
 pygame.init()
-class PaintGraph:
+
+
+class PaintGraph: #class for gui
     WIDTH = 759
     HEIGHT = 480
     scale_data_value = 0
@@ -11,65 +13,77 @@ class PaintGraph:
     scale_times = 0
     scale_value = 0.0
     window = pygame.display.set_mode((WIDTH, HEIGHT))
-    def init_graph(self,chars):
-        pygame.draw.line(self.window, (0, 255, 0), (35, 10), (35, self.HEIGHT-25))
-        pygame.draw.line(self.window, (0, 255, 0), (35, self.HEIGHT-25), (self.WIDTH-155,
-                         self.HEIGHT-25))
+
+    def init_graph(self, chars): #initialized graph with x axis values 
+        pygame.draw.line(self.window, (0, 255, 0), (35, 10),
+                        (35, self.HEIGHT-25))
+        pygame.draw.line(self.window, (0, 255, 0), (35, self.HEIGHT-25),
+                        (self.WIDTH-155, self.HEIGHT-25))
         font = pygame.font.SysFont(None, 25)
         x = 25
         for char in chars:
             text = font.render(char, True, (0, 255, 0))
             x += self.SCALE_WIDTH
-            self.window.blit(text,(x,458))
+            self.window.blit(text, (x, 458))
         pygame.display.flip()
-    def y_value(self):
+
+    def y_value(self): # method scales and creates y axis values
         font = pygame.font.SysFont(None, 25)
         i = 0
         if self.scale_times == 0:
             text = font.render(chr(48), True, (0, 255, 0))
-            self.window.blit(text,(10,445))
+            self.window.blit(text, (10, 445))
             text = font.render(chr(49), True, (0, 255, 0))
-            self.window.blit(text,(10,0))
+            self.window.blit(text, (10, 0))
         else:
             text = font.render("0.0", True, (0, 255, 0))
-            self.window.blit(text,(0,445))
+            self.window.blit(text, (0, 445))
             while i != self.scale_times:
                 i += 1
-                text = font.render(str(self.scale_value * i), True, (0, 255, 0))
-                self.window.blit(text,(0,445-(self.scale_height * i)))
+                text = font.render(str(self.scale_value * i), True,
+                                  (0, 255, 0))
+                self.window.blit(text, (0, 445-(self.scale_height * i)))
         pygame.display.flip()
-    def draw_lines(self, char_c):
-        a_coord = (0,0)
-        b_coord = (0,0)
+
+    def draw_lines(self, char_c): #main graph is drawn
+        a_coord = (0, 0)
+        b_coord = (0, 0)
         x = 25
         for i in char_c:
             x += self.SCALE_WIDTH
-            a_coord = (x,self.HEIGHT - (i*self.scale_data_value) - 25)
-            if b_coord != (0,0):
+            a_coord = (x, self.HEIGHT - (i*self.scale_data_value) - 25)
+            if b_coord != (0, 0):
         	pygame.draw.line(self.window, (0, 255, 0), b_coord, a_coord)
             b_coord = a_coord
         pygame.display.flip()
-    def more_info(self,char_c, files,chars):
+
+    def more_info(self,char_c, files,chars): #additional data is provided to user
         font = pygame.font.SysFont(None, 20)
-        text = font.render("Total letters:" + str(sum(char_c)), True, (0, 255, 0))
-        self.window.blit(text,(607,20))
-        text = font.render("Total files:" + str(len(files)), True, (0, 255, 0))
-        self.window.blit(text,(607,35))
+        text = font.render("Total letters:" + str(sum(char_c)), True,
+                          (0, 255, 0))
+        self.window.blit(text, (607, 20))
+        text = font.render("Total files:" + str(len(files)), True,
+                          (0, 255, 0))
+        self.window.blit(text, (607, 35))
         text = font.render("Letter repetitions", True, (0, 255, 0))
-        self.window.blit(text,(607,50))
+        self.window.blit(text, (607, 50))
         y = 50
         for i in chars:
             y += 15
-            text = font.render(i + "  " + str(char_c[chars.index(i)]), True, (0, 255, 0))
-            self.window.blit(text,(607,y))
+            text = font.render(i + "  " + str(char_c[chars.index(i)]), True,
+                               (0, 255, 0))
+            self.window.blit(text, (607, y))
         pygame.display.flip()
+
+
 graph = PaintGraph()
 files = glob.glob("/home/gintas/pyth/files/*.txt")
 characters = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
               "p","q","r","s","t","u","v","w","x","y","z")
 charcount = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
 for i in files:
-    file = open(i,"r")
+    file = open(i, "r")
     while True:
         char = file.read(1)
         if char == '':
@@ -78,7 +92,8 @@ for i in files:
         else:
             if char.lower() in characters:
                 charcount[characters.index(char.lower())] += 1
-if max(charcount) > 10:
+
+if max(charcount) > 10: # scaling
     graph.scale_data_value = float((graph.HEIGHT -35)) / max(charcount)
     graph.scale_height = 44
     graph.scale_times = 10
@@ -90,10 +105,12 @@ else:
     graph.scale_height = (graph.HEIGHT -35) / max(charcount)
     graph.scale_times = max(charcount)
     graph.scale_value = 1.0
+
 graph.init_graph(characters)
 graph.y_value()
 graph.draw_lines(charcount)
-graph.more_info(charcount,files,characters)
+graph.more_info(charcount, files, characters)
+
 while True: 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
